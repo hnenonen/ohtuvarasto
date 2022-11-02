703ff672e471918b2089855b17e39477d8fbb765
Nenonen Heikki A <nheikki@cubbli22-gold.local>
2022-11-02 08:23:00 +0200

    added unittests for varasto.py, coverage 100%

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -37,4 +37,35 @@ class TestVarasto(unittest.TestCase):
         self.varasto.ota_varastosta(2)
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
-        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)   
+
+    def test_varastosta_otetaan_liikaa(self):
+        varaston_saldo = self.varasto.saldo
+        self.varasto.ota_varastosta(varaston_saldo+1)
+
+    def test_varastoon_laitetaan_liikaa(self):
+        varastossa_tilaa = self.varasto.paljonko_mahtuu()
+        self.varasto.lisaa_varastoon(varastossa_tilaa+1)
+
+    def test_varastosta_otetaan_neg_maara(self):
+        saatu_maara = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_varastoon_lisataan_neg_maara(self):
+        varasto_saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, varasto_saldo)
+        
+    def test_luodaan_varasto_tilavuus_neg(self):
+        self.varasto = Varasto(-1)
+        self.assertAlmostEqual(self.varasto.tilavuus, 0)
+
+    def test_luodaan_varasto_saldo_neg(self):
+        self.varasto = Varasto(10, -1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test__str__(self):
+        saldo = self.varasto.saldo
+        tilaa = self.varasto.paljonko_mahtuu()
+        self.assertEqual(str(self.varasto), f"saldo = {saldo}, vielä tilaa {tilaa}")
+         
